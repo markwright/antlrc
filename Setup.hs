33ddf96b6f10de0bc2da2c855bdeb349723087d4
Mark Wright <markwright@internode.on.net>
2011-01-17 16:21:23 +1100

    Workaround http://hackage.haskell.org/trac/hackage/ticket/48 2. The .chi files need to be installed

--- Setup.hs
+++ Setup.hs
@@ -16,9 +16,9 @@ main = defaultMainWithHooks $ simpleUserHooks
     
 postCopyChi :: Args -> CopyFlags -> PackageDescription -> LocalBuildInfo -> IO ()
 postCopyChi args cflags pd lbi =
-  copyFiles deafening (fromFlag (copyDistPref cflags)) [((buildDir lbi) ++ "/Text/Antlrc/", "Lexer.chi")]  
+  copyFiles deafening (fromFlag (copyDistPref cflags)) [(buildDir lbi, "Text/Antlrc/Lexer.chi")]  
 
 postInstChi :: Args -> InstallFlags -> PackageDescription -> LocalBuildInfo -> IO ()
 postInstChi args iflags pd lbi = do
   let InstallDirs { libdir = libPref } = absoluteInstallDirs pd lbi NoCopyDest
-  installOrdinaryFiles deafening libPref [(buildDir lbi, "Lexer.chi")]
+  installOrdinaryFiles deafening libPref [(buildDir lbi, "Text/Antlrc/Lexer.chi")]
